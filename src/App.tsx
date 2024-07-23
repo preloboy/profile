356f059665ea02bfb398211a73ce37a93ce0517e
Prelo Tulshi <preloboy@gmail.com>
2024-07-23 14:10:23 +0530

    Initial Build

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Dashboard from './dashboard/Dashboard'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Dashboard/>
+    </>
+  )
+}
+
+export default App
